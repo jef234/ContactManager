d3ccdf388aabe40656848ed9f3e22cc8907e06b4
jr051323 <jeffrey.rajkumar@cerner.com>
2020-05-04 23:58:16 +0530

    Provider and Reducer

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from 'react';
 import Contacts from './Components/Contacts';
 import Header from './Components/Header';
 
+import { Provider } from './Provider/context'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header branding="Contact Manager" />
-      <div className="container">
-        <Contacts />
+    <Provider>
+      <div className="App">
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contacts />
+        </div>
       </div>
-    </div>
+    </Provider>
   )
 }
 
