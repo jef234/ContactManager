5e634c1000f3f5cedf1fb9cfb14e24938c733c90
jr051323 <jeffrey.rajkumar@cerner.com>
2020-05-06 11:03:43 +0530

    Updates

---
+++ precache-manifest.d6dccd23d518623ab07a232bdd2e7a2f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9236222e344218bba036b97ddca0b7ce",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "a04c5d45216daa243741",
+    "url": "/ContactManager/static/css/2.1b50e56c.chunk.css"
+  },
+  {
+    "revision": "1dc97f1481b970cce459",
+    "url": "/ContactManager/static/css/main.2e9daa2a.chunk.css"
+  },
+  {
+    "revision": "a04c5d45216daa243741",
+    "url": "/ContactManager/static/js/2.6b4a4122.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ContactManager/static/js/2.6b4a4122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dc97f1481b970cce459",
+    "url": "/ContactManager/static/js/main.502af984.chunk.js"
+  },
+  {
+    "revision": "112dedea4ee41fec0345",
+    "url": "/ContactManager/static/js/runtime-main.06b61ea7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ContactManager/static/media/logo.5d5d9eef.svg"
+  }
+]);
