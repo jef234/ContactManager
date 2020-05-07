0eae291d29eeec1c33b82d3e4ce7e392fd6ea125
jr051323 <jeffrey.rajkumar@cerner.com>
2020-05-07 11:18:09 +0530

    About page added developer info

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -5,8 +5,9 @@ export default class About extends Component {
         return (
             <div>
                 <h2 className="display-4 mb-3"><span className="text-danger">About</span> Contact Manager</h2>
-                <p className="lead">Simple app to mange contacts</p>
-                <p className="text-secondary">Version 1.0.0</p>
+                <p className="lead">Simple app to manage contacts</p>
+                <p className="text-secondary">Developer : Jeffrey Rajkumar</p>
+                <p className="text-secondary">Version : 1.0.0</p>
             </div>
         )
     }
